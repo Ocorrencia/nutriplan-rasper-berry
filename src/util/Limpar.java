9742680ea1421f2fcfd68a40125bfe9ef840d936
diogo.melo <diogo.melo@teste-ti.nutriplan.com.br>
2018-01-02 11:22:30 -0200

    alteracoes

---
+++ src/util/Limpar.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import static com.alee.managers.notification.NotificationIcon.file;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Limpar {
+
+    EnviarEmail enviarEmail = new EnviarEmail();
+
+    public void executeSqlScript() {
+        try {
+            // Delimiter
+            String delimiter = ";";
+            // Create scanner
+            Scanner scanner;
+            InputStream reader = (getClass().getResourceAsStream("/arquivos/REFRESH.sql"));
+            scanner = new Scanner(reader).useDelimiter(delimiter);
+            // Loop through the SQL file statements 
+            Statement currentStatement = null;
+            while (scanner.hasNext()) {
+                // Get statement 
+                String rawStatement = scanner.next() + delimiter;
+                try {
+                    // Execute statement
+                    currentStatement = ConexaoMysql.getConexaoMySQL().createStatement();
+                    currentStatement.executeUpdate(rawStatement);
+                } catch (SQLException e) {
+                    if (e.getMessage().contains("Query was empty")) {
+                        Notificacao.infoBox("Banco Reinicializado com Sucesso!", true);
+                    }else{
+                      enviarEmail.enviaEmail(e.getMessage(), "Erro ao realizar refresh");
+                    }
+                } finally {
+                    // Release resources
+                    if (currentStatement != null) {
+                        try {
+                            currentStatement.close();
+                        } catch (SQLException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    currentStatement = null;
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
